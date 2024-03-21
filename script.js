2d4ebc8172f33a263419775f91117ba88854bce9
kingmanawat <kingmanawat@gmail.com>
2024-03-21 11:47:18 +0530

    new

--- script.js
+++ script.js
@@ -1,36 +1,15 @@
-// var slideIndex =0 ;
-// showDivs(slideIndex);
-
-// function Slide(n) {
-//   showDivs(slideIndex += n);
-// }
-// function showDivs(n) {
-//   var i;
-//   let x = document.getElementsByTagName('img');
-//   if (n > x.length) {slideIndex = 1}
-//   if (n < 1) {slideIndex = x.length}
-//   for (i = 0; i < x.length; i++) {
-//     x[i].style.display = "none";  
-//   } 
-//   x[slideIndex-1].style.display = "block"; 
-//   setInterval(showDivs,1000);
-// }
 let slideIndex = 0;
 showSlides();
 
 function showSlides() {
   let i;
   let slides = document.getElementsByTagName("img");
-  let dots = document.getElementsByClassName("dot");
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";  
   }
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1}    
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
   slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
+ 
+  setTimeout(showSlides, 2000);
 }
