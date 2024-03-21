964b20d4b1a011b49c98431e9e327bc1bb576960
kingmanawat <kingmanawat@gmail.com>
2024-03-21 11:13:35 +0530

    new version

---
+++ script.js
@@ -0,0 +1,17 @@
+var slideIndex =0 ;
+showDivs(slideIndex);
+
+function Slide(n) {
+  showDivs(slideIndex += n);
+}
+function showDivs(n) {
+  var i;
+  let x = document.getElementsByTagName('img');
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  } 
+  x[slideIndex-1].style.display = "block"; 
+  
+}
