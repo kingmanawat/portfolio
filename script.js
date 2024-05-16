c9028fea0b09b4d4ad565101ecf8267c254ceb95
kingmanawat <kingmanawat@gmail.com>
2024-05-16 20:56:29 +0530

    update

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 
 function random()
 {
-    const imgs =['url("king1.jpeg")','url("king2.jpg")'];
+    const imgs =['url("king2.jpg")','url("king3.png")','url("king4.png")'];
     const pic = document.querySelector('section')
     const a= imgs[Math.floor(Math.random()*imgs.length)];
     
     pic.style.backgroundImage = a ;
 }
 
-setInterval(random,2000);
+setInterval(random,1000);
 
 (function () {
-    var words = ["Photographer","Developer","Youtuber","Happy Person" ],
+    var words = ["Photographer","Developer","Youtuber","Happy Person","Farmer","Rider" ],
     i = 0;
     setInterval(function(){ $('#words').fadeOut(function(){
         $(this).html(words[(i = (i + 1) % words.length)]).fadeIn();
