871180e31814c7203c4e20d8fdfa124fb7e8e0fb
kingmanawat <kingmanawat@gmail.com>
2024-05-19 22:30:49 +0530

    hello

--- script.js
+++ script.js
@@ -1,19 +1,10 @@
+const navBar=document.querySelector('.navbar');
 
-function random()
-{
-    const imgs =['url("king2.jpg")','url("king3.png")','url("king4.png")'];
-    const pic = document.querySelector('section')
-    const a= imgs[Math.floor(Math.random()*imgs.length)];
-    
-    pic.style.backgroundImage = a ;
-}
-
-setInterval(random,1000);
-
-(function () {
-    var words = ["Photographer","Developer","Youtuber","Happy Person","Farmer","Rider" ],
-    i = 0;
-    setInterval(function(){ $('#words').fadeOut(function(){
-        $(this).html(words[(i = (i + 1) % words.length)]).fadeIn();
-      }); }, 2000)
-  })();
+window.addEventListener('scroll',() => {
+        if(window.scrollY >100){
+            navBar.classList.add('navbar-scroll');
+        }
+        else if(window.scrollY <100){
+            navBar.classList.remove('navbar-scroll');
+        }
+});
